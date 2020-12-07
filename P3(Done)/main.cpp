e81dcdd8dc3a464b810b412929846237640055e6
DOGGY-SAINT <1123117528@qq.com>
2020-12-07 13:59:44 +0800

    Update P3

--- P3(Done)/main.cpp
+++ P3(Done)/main.cpp
@@ -7,55 +7,81 @@
 
 using namespace std;
 
-#define WALL     '#'
-#define EMPTY    '0'
+#define WALL '#'
+#define EMPTY '0'
 #define ENTRANCE '1'
-#define EXIT     '2'
-#define SIZE      7
-#define OPPOSITE(x) 3-x
+#define EXIT '2'
+#define SIZE 7
+#define OPPOSITE(x) 3 - x
+
+const char defaultmap1[SIZE][SIZE] =
+	{{'#', '#', '#', '#', '#', '#', '#'},
+	 {'#', '1', '#', '0', '0', '0', '#'},
+	 {'#', '0', '#', '0', '#', '#', '#'},
+	 {'#', '0', '0', '0', '#', '#', '#'},
+	 {'#', '0', '#', '0', '0', '0', '#'},
+	 {'#', '0', '#', '0', '#', '2', '#'},
+	 {'#', '#', '#', '#', '#', '#', '#'}};
+
+const char defaultmap2[SIZE][SIZE] =
+	{{'#', '#', '#', '#', '#', '#', '#'},
+	 {'#', '1', '#', '0', '0', '0', '#'},
+	 {'#', '0', '#', '0', '#', '#', '#'},
+	 {'#', '0', '0', '0', '#', '#', '#'},
+	 {'#', '0', '#', '0', '0', '#', '#'},
+	 {'#', '0', '#', '0', '#', '2', '#'},
+	 {'#', '#', '#', '#', '#', '#', '#'}};
+
 
 class Position
 {
-public:
+	friend class Solution;
+	friend class Map;
+private:
 	int _x, _y;
+public:
 	Position()
-	{}
-	Position(int x, int y) :_x(x), _y(y)
-	{}
+	{
+	}
+	Position(int x, int y) : _x(x), _y(y)
+	{
+	}
 	bool isInside()
 	{
-		return _x >= 0 && _y >= 0 && _x < SIZE&& _y < SIZE;
+		return _x >= 0 && _y >= 0 && _x < SIZE && _y < SIZE;
 	}
-	Position operator+(const Position& p2)const
+	Position operator+(const Position &p2) const
 	{
 		return Position(p2._x + _x, p2._y + _y);
 	}
-	bool operator==(const Position& p2)const
+	bool operator==(const Position &p2) const
 	{
 		return _x == p2._x && _y == p2._y;
 	}
-	friend ostream& operator<<(ostream& os, const Position& pos)
+	friend ostream &operator<<(ostream &os, const Position &pos)
 	{
 		os << "<" << pos._x << "," << pos._y << ">";
 		return os;
 	}
 };
-
-const Position Direction[4] = { {-1,0}, {0,-1}, {0,1}, {1,0} };
+//左，上，下，右
+const Position Direction[4] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
 
 class Solution
 {
 private:
 	MyVector<Position> _solution;
 	bool _arrive;
+
 public:
 	Solution()
-	{}
+	{
+	}
 	void setArrive(bool arrive)
 	{
 		_arrive = arrive;
 	}
-	bool arrive()const
+	bool arrive() const
 	{
 		return _arrive;
 	}
@@ -63,13 +89,18 @@ public:
 	{
 		return _solution.empty();
 	}
-	Solution operator+(const Position& pos)const
+	void clear()
+	{
+		_solution.clear();
+		_arrive=false;
+	}
+	Solution operator+(const Position &pos) const
 	{
 		Solution res = *this;
 		res._solution.push_back(pos);
 		return res;
 	}
-	Solution operator+=(const Position& pos)
+	Solution operator+=(const Position &pos)
 	{
 		_solution.push_back(pos);
 		return *this;
@@ -79,14 +110,14 @@ public:
 		_solution.pop_back();
 		return *this;
 	}
-	friend ostream& operator<<(ostream& os, Solution& solution)
+	friend ostream &operator<<(ostream &os, Solution &solution)
 	{
 		for (auto it = solution._solution.begin();;)
 		{
 			os << *it;
 			if (++it != solution._solution.end())
 				os << " ---> ";
-			else 
+			else
 				break;
 		}
 		return os;
@@ -100,15 +131,27 @@ private:
 	Position _entrance, _exit;
 	Solution _solution;
 public:
-	static char defaultmap[SIZE][SIZE];
 	Map()
 	{
 		for (int i = 0; i < SIZE; i++)
 			for (int j = 0; j < SIZE; j++)
-				_map[i][j] = defaultmap[i][j];
+				_map[i][j] = defaultmap1[i][j];
 		_entrance = Position(1, 1), _exit = Position(5, 5);
 	}
-	Map(char map[SIZE][SIZE])
+	Map(const char map[SIZE][SIZE])
+	{
+		for (int i = 0; i < SIZE; i++)
+			for (int j = 0; j < SIZE; j++)
+			{
+				if (map[i][j] == ENTRANCE)
+					_entrance = Position(i, j);
+				else if (map[i][j] == EXIT)
+					_exit = Position(i, j);
+				_map[i][j] = map[i][j];
+			}
+	}
+	//设置地图
+	void setMap(const char map[SIZE][SIZE])
 	{
 		for (int i = 0; i < SIZE; i++)
 			for (int j = 0; j < SIZE; j++)
@@ -123,11 +166,11 @@ public:
 	//展示地图
 	void showMap()
 	{
-		cout << "地图(起点为1终点为2):" << endl;
+		cout <<endl<< "地图(起点为1终点为2):" << endl;
 		cout << ' ' << "    ";
 		for (int i = 0; i < SIZE; i++)
 		{
-				cout << i<< "列  ";
+			cout << i << "列  ";
 		}
 		cout << endl;
 		for (int i = 0; i < SIZE; i++)
@@ -141,17 +184,18 @@ public:
 	//寻路
 	bool findSolution()
 	{
+		_solution.clear();
 		_solution = findNextStep(_entrance, _solution, -1);
 		if (_solution.arrive())
 			return true;
 		return false;
 	}
-	char operator[](Position pos)const
+	char operator[](Position pos) const
 	{
 		return _map[pos._x][pos._y];
 	}
 	//递归
-	Solution findNextStep(Position pos, Solution solution, int lastdir)const
+	Solution findNextStep(Position pos, Solution solution, int lastdir) const
 	{
 		solution += pos;
 		if (pos == _exit)
@@ -159,7 +203,7 @@ public:
 			solution.setArrive(true);
 			return solution;
 		}
-		Solution newSolution=solution;
+		Solution newSolution = solution;
 		for (int dir = 0; dir < 4; dir++)
 		{
 			if (dir == OPPOSITE(lastdir))
@@ -177,24 +221,26 @@ public:
 	}
 	void showSolution()
 	{
-		cout << "路径:" << endl;
-		cout << _solution<<endl;
+		if (_solution.arrive())
+		{
+			cout << "路径:" << endl;
+			cout << _solution << endl;
+		}
+		else
+		{
+			cout << "路径不存在" << endl;
+		}
 	}
 };
 
-//默认地图
-char Map::defaultmap[SIZE][SIZE] =
-{ {'#','#','#','#','#','#','#'},
-  {'#','1','#','0','0','0','#'},
-  {'#','0','#','0','#','#','#'},
-  {'#','0','0','0','#','#','#'},
-  {'#','0','#','0','0','0','#'},
-  {'#','0','#','0','#','2','#'},
-  {'#','#','#','#','#','#','#'} };
 
 int main()
 {
 	Map map;
+	map.showMap();
+	map.findSolution();
+	map.showSolution();
+	map.setMap(defaultmap2);
 	map.showMap();
 	map.findSolution();
 	map.showSolution();
